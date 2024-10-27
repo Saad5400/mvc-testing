a6b450a8941dfdd51640223dac6554ba34b08e34
Hamad <hmdalwasli06@gmail.com>
2024-10-27 10:49:23 +0300

    Reset last Saad Batwa Branch Because some errors

--- src/main/java/org/example/mvctodo/controller/ToDoController.java
+++ src/main/java/org/example/mvctodo/controller/ToDoController.java
@@ -57,7 +57,7 @@ public class ToDoController {
         toDoService.markAsPending(id);
         return "redirect:/";
     }
-    //
+    // Delete a To-Do
     // Delete a To-Do
     @GetMapping("/delete/{id}")
     public String deleteToDo(@PathVariable Long id) {
